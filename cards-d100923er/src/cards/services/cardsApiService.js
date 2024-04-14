cc1e3e86c1707ba48420ba1fc4f025e4f0e832cd
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-04-14 12:26:53 +0300

    CREATE cardsApiService.js + usersApiService.js

---
+++ cards-d100923er/src/cards/services/cardsApiService.js
@@ -0,0 +1,9 @@
+export default function cardsApiService() {
+  const getCards = ([]) => {
+    return null;
+  };
+  const getCard = (cardId) => {
+    return null;
+  };
+  return { getCards, getCard };
+}
---
+++ cards-d100923er/src/users/services/usersApiService.js
@@ -0,0 +1,12 @@
+export default function usersApiService() {
+  const login = () => {
+    return null;
+  };
+  const signup = (user) => {
+    return null;
+  };
+  const getUserData = (id) => {
+    return null;
+  };
+  return { login, signup, getUserData };
+}
