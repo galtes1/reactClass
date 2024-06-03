3380195c893f7eb9b3a37a419a3d2463e6e9a74f
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-06-03 16:59:12 +0300

    update

--- cards-d100923er/src/providers/CustomThemeProvider.jsx
+++ cards-d100923er/src/providers/CustomThemeProvider.jsx
@@ -5,29 +5,43 @@ import CssBaseline from "@mui/material/CssBaseline";
 const ThemeContext = createContext();
 
 export default function CustomThemeProvider({ children }) {
- const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useState(false);
 
- const toggleDarkMode = useCallback(() => {
-  setIsDark((prev) => !prev);
- }, []);
+  const toggleDarkMode = useCallback(() => {
+    setIsDark((prev) => !prev);
+  }, []);
 
- const theme = createTheme({
-  palette: {
-   mode: isDark ? "dark" : "light",
-  },
- });
- return (
-  <ThemeProvider theme={theme}>
-   <CssBaseline />
-   <ThemeContext.Provider value={{ toggleDarkMode, isDark }}>
-    {children}
-   </ThemeContext.Provider>
-  </ThemeProvider>
- );
+  const theme = createTheme({
+    palette: {
+      mode: isDark ? "dark" : "light",
+    },
+    typography: {
+      h3: {
+        fontSize: "3rem",
+        "@media (max-width:480px)": {
+          fontSize: "1.75rem",
+        },
+      },
+      h5: {
+        fontSize: "1.5rem",
+        "@media (max-width:480px)": {
+          fontSize: "1rem",
+        },
+      },
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ThemeContext.Provider value={{ toggleDarkMode, isDark }}>
+        {children}
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
 }
 
 export const useTheme = () => {
- const context = useContext(ThemeContext);
- if (!context) throw new Error("useTheme must be used within a Provider");
- return context;
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error("useTheme must be used within a Provider");
+  return context;
 };
