70c26217fb9550410966f87387eb586e522c1148
galtes1 <155002780+galtes1@users.noreply.github.com>
2024-06-09 17:45:02 +0300

    cardsProject final push

--- cards-d100923er/src/cards/pages/CustomCardsPage.jsx
+++ cards-d100923er/src/cards/pages/CustomCardsPage.jsx
@@ -7,39 +7,49 @@ import CustomNewCardButton from "../components/CustomNewCardButton";
 import { useUser } from "../../users/providers/UserProvider";
 
 export default function CustomCardsPage() {
- const { value, getAllCards, handleCardDelete, handleCardLike } = useCards();
- const { error, isLoading, filteredCards } = value;
- const {user} = useUser()
+  const { value, getAllCards, handleCardDelete, handleCardLike } = useCards();
+  const { error, isLoading, filteredCards } = value;
+  const { user } = useUser();
 
- useEffect(() => {
-  getAllCards();
- }, [getAllCards]);
+  useEffect(() => {
+    getAllCards();
+  }, [getAllCards]);
 
- const handleDelete = async (cardId) => {
-  await handleCardDelete(cardId);
-  getAllCards();
- };
+  const handleDelete = async (cardId) => {
+    await handleCardDelete(cardId);
+    getAllCards();
+  };
 
- const handleLike = async (cardId) => {
-  await handleCardLike(cardId);
- };
+  const handleLike = async (cardId) => {
+    await handleCardLike(cardId);
+  };
 
- if (user&& ((user.isBusiness===true)||(user.isAdmin===true)))
- return (
-  <div>
-   <CustomPageHeader title="Cards" subtitle="All Available Cards" />
-   <CustomCardsFeedback
-    cards={filteredCards}
-    isLoading={isLoading}
-    handleDelete={handleDelete}
-    handleLike={handleLike}
-    error={error}
-   />
-   <CustomNewCardButton />
-  </div>
-   );
-   else{
-
-    
-   }
+  if (user && (user.isBusiness === true || user.isAdmin === true))
+    return (
+      <div>
+        <CustomPageHeader title="Cards" subtitle="All Available Cards" />
+        <CustomCardsFeedback
+          cards={filteredCards}
+          isLoading={isLoading}
+          handleDelete={handleDelete}
+          handleLike={handleLike}
+          error={error}
+        />
+        <CustomNewCardButton />
+      </div>
+    );
+  else {
+    return (
+      <div>
+        <CustomPageHeader title="Cards" subtitle="All Available Cards" />
+        <CustomCardsFeedback
+          cards={filteredCards}
+          isLoading={isLoading}
+          handleDelete={handleDelete}
+          handleLike={handleLike}
+          error={error}
+        />
+      </div>
+    );
+  }
 }
